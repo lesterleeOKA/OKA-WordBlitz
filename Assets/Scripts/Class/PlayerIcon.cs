c669e5843d034ba5dde8aa93b48da33262ef14c0
LAPTOP-V3JB56OU\lesterleeOKA <lesterleeopenknowledgehk@gmail.com>
2024-09-26 18:12:18 +0800

    updated loadQuestionFile function for auto detect load question by api or question json

--- Assets/Scripts/Class/PlayerIcon.cs
+++ Assets/Scripts/Class/PlayerIcon.cs
@@ -7,10 +7,17 @@ public class PlayerIcon : MonoBehaviour
     public Color32 playerColor = Color.black;
     public Image[] playerIcons;
     public TextMeshProUGUI[] playerNames, playerNos;
+    public CanvasGroup[] playerNameBoxes;
     public Image[] PlayerIcons
     {
         get { return this.playerIcons; }
         set { this.playerIcons = value; }
+    }
+
+    public CanvasGroup[] PlayerNameBoxes
+    {
+        get { return this.playerNameBoxes; }
+        set { this.playerNameBoxes = value; }
     }
 
     public TextMeshProUGUI[] PlayerNames
@@ -31,8 +38,18 @@ public class PlayerIcon : MonoBehaviour
         if(_status) { 
             for (int i = 0; i < this.PlayerNames.Length; i++)
             {
-                if (this.PlayerNames[i] != null)
-                    this.PlayerNames[i].text = _playerName;
+                if (this.PlayerNames[i] != null && this.PlayerNameBoxes[i] != null) {
+
+                    if (!string.IsNullOrEmpty(_playerName))
+                    {
+                        SetUI.Set(this.PlayerNameBoxes[i], true, 0f);
+                        this.PlayerNames[i].text = _playerName;
+                    }
+                    else
+                    {
+                        SetUI.Set(this.PlayerNameBoxes[i], false, 0f);
+                    }
+                }
             }
 
             for (int i = 0; i < this.PlayerIcons.Length; i++)
